8246350c35e4c99be9eac06c02b65df65bc0d063
Maixme Laroche <mlaroche2009@gmail.com>
2022-04-24 08:54:15 -0400

    updated used data

--- filter.py
+++ filter.py
@@ -9,15 +9,22 @@ def _removeTFSA(df: pd.DataFrame) -> pd.DataFrame:
 def _removeUnsupportedData(df: pd.DataFrame) -> pd.DataFrame:
     df = _removeTFSA(df)
     
-    df = df.drop(df[df["Action"] == "BRW"].index)
-    df = df.drop(df[df["Action"] == "CON"].index)
-    df = df.drop(df[df["Action"] == "DEP"].index)
-    df = df.drop(df[df["Action"] == "DIS"].index)
-    df = df.drop(df[df["Action"] == "FEE"].index)
-    df = df.drop(df[df["Action"] == "FXT"].index)
-    df = df.drop(df[df["Action"] == "CIL"].index)
-    df = df.drop(df[df["Action"] == "FCH"].index)
-    df = df.drop(df[df["Action"] == "TF6"].index)
+    df = df.drop(df[df["Action"] == "BRW"].index) # only occured in 2020
+    df = df.drop(df[df["Action"] == "CON"].index) # TFSA Contribution. Not a taxable event
+    df = df.drop(df[df["Action"] == "DEP"].index) # Deposit. Not a taxable event
+    df = df.drop(df[df["Action"] == "EFT"].index) # Electronic fund transfer. Not a taxable event
+    df = df.drop(df[df["Action"] == "DIS"].index) # 2020 term for dividend
+    df = df.drop(df[df["Action"] == "FEE"].index) # only occured in 2020 (Borrow fees)
+    df = df.drop(df[df["Action"] == "FXT"].index) # Forein exchange, only done in 2020
+    df = df.drop(df[df["Action"] == "CIL"].index) # 2020 term for ADJ
+    df = df.drop(df[df["Action"] == "FCH"].index) # Only on 2020 (Data fees, rebates etc)
+    df = df.drop(df[df["Action"] == "TF6"].index) # Only happened in TFSA
+    df = df.drop(df[df["Action"] == "REV"].index) # 2020 SQQQ reverse split (position closed in 2020)
+    df = df.drop(df[df["Activity Type"] == "Interest"].index) # only in 2020, not a transaction
+    df = df.drop(df[df["Activity Type"] == "Dividends"].index) # Dividends and interests
+    df = df.drop(df[df["Action"] == "MKT"].index) # only in 2020, not a transaction
+    
+    
     return df
 
 def _removeUnusedCol(df: pd.DataFrame) -> pd.DataFrame:
@@ -27,4 +34,5 @@ def _removeUnusedCol(df: pd.DataFrame) -> pd.DataFrame:
 def removeBadData(df: pd.DataFrame)-> pd.DataFrame:
     df = _removeUnsupportedData(df)
     df = _removeUnusedCol(df)
+    # df.to_excel("Input copy.xlsx", index = None)
     return df
