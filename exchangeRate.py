e5373b335f73f84ae965ddacef95147631a7e6da
Maixme Laroche <mlaroche2009@gmail.com>
2022-04-26 10:10:45 -0400

    exchange rate autoDownload

--- exchangeRate.py
+++ exchangeRate.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pandas as pd
 import yfinance as yf
-
+import requests
 
 
 
@@ -23,13 +23,13 @@ class YahooRate:
             ticker = yf.Ticker(self.currency)
             try:
                 prices = ticker.history(period='max')['Close']
-                print(prices)
                 df = pd.DataFrame(prices, index=prices.index)
                 self.data = df
                 df.to_csv(folder + self.currency + '.csv', index=True)
             except Exception as e:
                 print(e)
                 print("error getting currency data for " + self.currency)
+
     def getRate(self, currency: str, date: datetime, precision = DAILY):
         if currency == 'CAD':
             return 1
@@ -55,17 +55,32 @@ class BankOfCanadaRate:
     DAILY = 'FXUSDCAD'
     _YEARLY_FILE_NAME = 'FX_RATES_ANNUAL.csv'
     _DAILY_FILE_NAME = 'FX_RATES_DAILY.csv'
+    _DAILY_URL = 'https://www.banqueducanada.ca/valet/observations/group/FX_RATES_DAILY/csv'
+    _YEARLY_URL = 'https://www.banqueducanada.ca/valet/observations/group/FX_RATES_ANNUAL/csv'
     backupRate = YahooRate()
+    folder = 'market_data/'
     def __init__(self, dailyLabel = 'FXUSDCAD', yearlyLabel = 'FXAUSDCAD') -> None:
-        folder = 'market_data/'
         self.dailyLabel = dailyLabel
         self.yearlyLabel = yearlyLabel
 
         try:
-            self.daily = pd.read_csv(folder + BankOfCanadaRate._DAILY_FILE_NAME, index_col='date')
-            self.yearly = pd.read_csv(folder + BankOfCanadaRate._YEARLY_FILE_NAME, index_col='date')
+            self.readFiles()
         except Exception as e:
-            raise e
+            self.fetchFromURL()
+            
+    def readFiles(self):
+        self.daily = pd.read_csv(BankOfCanadaRate.folder + BankOfCanadaRate._DAILY_FILE_NAME, index_col='date')
+        self.yearly = pd.read_csv(BankOfCanadaRate.folder + BankOfCanadaRate._YEARLY_FILE_NAME, index_col='date')
+    def fetchFromURL(self):
+        dailyRes = requests.get(BankOfCanadaRate._DAILY_URL)
+        content = dailyRes.content
+        content = content.split(b'"OBSERVATIONS"\n')[1]
+        open(BankOfCanadaRate.folder + BankOfCanadaRate._DAILY_FILE_NAME, "wb").write(content)
+
+        yearlyRes = requests.get(BankOfCanadaRate._YEARLY_URL)
+        content = yearlyRes.content.split(b'"OBSERVATIONS"\n')[1]
+        open(BankOfCanadaRate.folder + BankOfCanadaRate._YEARLY_FILE_NAME, "wb").write(content)
+        self.readFiles()
         
     def getRate(self, currency: str, date: datetime, precision = YEARLY):
         if currency == 'CAD':
@@ -78,6 +93,7 @@ class BankOfCanadaRate:
         except:
             return self.backupRate.getRate(currency, date, precision)
     def _getDailyRate(self, date: datetime):
+        self.checkMax(self.daily,date)
         rate = self.daily.loc[str(date.date()),self.dailyLabel]
         return rate
 
@@ -85,7 +101,12 @@ class BankOfCanadaRate:
         beginingOfYear = datetime(year=date.year, month=1, day=1)
         if beginingOfYear.year >= datetime.now().year:
             return self._getDailyRate(date)
+        self.checkMax(self.yearly, beginingOfYear)
         rate = self.yearly.loc[str(beginingOfYear.date()),self.yearlyLabel]
         return rate
-
+    def checkMax(self, df: pd.DataFrame, date: datetime):
+        max = df.index[-1]
+        maxDate =  datetime.strptime(max, '%Y-%m-%d')
+        if date > maxDate and date < datetime.now():
+            self.fetchFromURL()
 
