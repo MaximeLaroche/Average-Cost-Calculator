8e2cebb851d4022b86136e15d7ba037a92f3a8d1
Maixme Laroche <mlaroche2009@gmail.com>
2022-04-27 07:08:25 -0400

    update main.sh

--- main.sh
+++ main.sh
@@ -9,11 +9,12 @@ done < $filename
 rm $filename
 
 # run the script
+git pull
 python -m pip install -r requirements.txt
 python main.py
 
 # Open excel sheets
-"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" Option.xlsx & stk=$!
+"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" Options.xlsx & stk=$!
 "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" Stocks.xlsx & opt=$!
 echo -e $stk >> $filename
 echo -e $opt >> $filename
